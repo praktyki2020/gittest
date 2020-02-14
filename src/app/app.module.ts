c243c9f4df71bdb7e74c3e5a9f480c8359a55f45
Oskar <dreaddoll1204@gmail.com>
2020-02-14 13:58:47 +0100

    update 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IntroHeaderComponent } from './components/intro-header/intro-header.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IntroHeaderComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
