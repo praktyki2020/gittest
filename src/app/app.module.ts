23d654cf4866b323827484b526808c9a930385ca
Oskar <dreaddoll1204@gmail.com>
2020-02-14 16:29:10 +0100

    Fix components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IntroHeaderComponent } from './components/intro-header/intro-header.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ContactFormComponent } from './components/contact-form/contact-form.component';
-
 import { PostCardComponent } from './components/post-card/post-card.component';
 import { FooterComponent } from './components/footer/footer.component';
 
@@ -14,8 +11,6 @@ import { FooterComponent } from './components/footer/footer.component';
   declarations: [
     AppComponent,
     IntroHeaderComponent,
-    HeaderComponent,
-    ContactFormComponent,
     PostCardComponent,
     FooterComponent,
   ],
