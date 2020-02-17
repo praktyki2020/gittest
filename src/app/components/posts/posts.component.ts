9cbff09f4a99ac94b119dae76a8146e359292172
Grzegorz Szymonik <grzegorz.szymonik@devspire.eu>
2020-02-17 16:44:47 +0100

    posts example

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { postsData } from './../../data/posts';
 
 @Component({
     selector: 'app-posts',
@@ -7,5 +8,8 @@ import { Component } from "@angular/core";
 })
 export class PostsComponent {
     
+    listaPostow = postsData;
 
+
+    
 }
---
+++ src/app/data/posts.ts
@@ -0,0 +1,38 @@
+export const postsData = [
+    {
+      name: 'Post 1',
+      description: 'Jakis opis postu 1'
+    },
+    {
+      name: 'Post 2',
+      description: 'Jakis opis postu 2'
+    },
+    {
+      name: 'Post 3',
+      description: 'Jakis opis postu 3'
+    },
+    {
+      name: 'Post 3',
+      description: 'Jakis opis postu 3'
+    },
+    {
+      name: 'Post 3',
+      description: 'Jakis opis postu 3'
+    },
+    {
+      name: 'Post 3',
+      description: 'Jakis opis postu 3'
+    },
+    {
+      name: 'Post 3',
+      description: 'Jakis opis postu 3'
+    },
+    {
+      name: 'Post 3',
+      description: 'Jakis opis postu 3'
+    },
+    {
+      name: 'Post 3',
+      description: 'Jakis opis postu 3'
+    },
+  ];
