64e4fb2b1eb91d74bc7075c7f839d75cdbe71938
Oskar <dreaddoll1204@gmail.com>
2020-02-17 12:44:28 +0100

    Routing 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,33 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IntroHeaderComponent } from './components/intro-header/intro-header.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ContactFormComponent } from './components/contact-form/contact-form.component';
-
 import { PostCardComponent } from './components/post-card/post-card.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ServicesComponent } from './components/services/services.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    // test test test
     IntroHeaderComponent,
-    HeaderComponent,
-    ContactFormComponent,
-    // raz dwa trzy
     PostCardComponent,
     FooterComponent,
     // komentarz bla bla
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: '', component: ServicesComponent },
+      { path: '', component: PostsComponent },
+      { path: '', component: ContactComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
